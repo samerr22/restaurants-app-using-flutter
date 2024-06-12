ece4119188aba44b7fa0c82d2c9603f02be618eb
sameeera <laksameera5@gmail.com>
2024-06-12 21:28:42 +0530

    auth

---
+++ packages/user_repository/lib/src/models/user.dart
@@ -0,0 +1,31 @@
+export 'user.dart';
+
+class MyUser{
+  String userId;
+  String email;
+  String name;
+  bool hasActiveCart;
+
+  MyUser({
+    required this.userId,
+    required this.email,
+    required this.name,
+    required this.hasActiveCart,
+  });
+
+  static final empty = MyUser(
+    userId: '',
+   email: '',
+    name: '',
+   hasActiveCart: false,);
+
+   MyUserEntity toEntity() {
+    return MyUserEntity(
+      userId: userId, 
+      email: email, 
+      name: name, 
+      hasActiveCart: hasActiveCart,);
+   }
+
+
+}
